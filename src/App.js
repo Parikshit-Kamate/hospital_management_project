33e6518b7be3a51788e75a991df2080031438241
Parikshit-Kamate <parikshitkamate002@gmail.com>
2025-05-27 10:14:48 +0530

    changes in app.js file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';        
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';           
 import Home from './Pages/Home.jsx';
 import About from './Pages/About.jsx';
 import Services from './Pages/Services.jsx';
